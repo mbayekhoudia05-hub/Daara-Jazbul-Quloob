7c3db316126d4b9ca6ec2ceb26e551c18a96b243
mbayekhoudia05-hub <mbayekhoudia05@gmail.com>
2026-09-26 16:13:03 +0000

    Update Supabase publishable key in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = "https://wgypltnlbnqvfbqjzypd.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "COLLER_ICI_LA_PUBLISHABLE_KEY";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_2anYkmR_o8F1KA-wrgGMCA_1fVr3W0k";
 window.DJQ_CONFIG = { SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY };
